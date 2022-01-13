7297f61272fd861e3c64df9b8768194c1e1fa039
tharitthaveekittikul <s6301012630079@email.kmutnb.ac.th>
2022-01-13 22:46:29 +0700

    add moredetails

--- authenticate.php
+++ authenticate.php
@@ -44,31 +44,4 @@ else{
     header("Location: index.php?msg=incorrect");
     exit();
 }
-// if ( !isset($_POST['username'], $_POST['password']) ) {
-// 	exit('Please fill both the username and password fields!');
-// }
-
-// if($stmt = $con->prepare('SELECT * FROM admin_user WHERE username = ?')) {
-//     $stmt->bind_param('s', $_POST['username']);
-//     $stmt->execute();
-//     $stmt->store_result();
-//     if ($stmt->num_rows > 0) {
-//         $stmt->bind_result($id, $password);
-//         $stmt->fetch();
-//         if (password_verify($_POST['password'], $password)) {
-//             session_regenerate_id();
-//             $_SESSION['loggedin'] = TRUE;
-//             $_SESSION['username'] = $_POST['username'];
-//             $_SESSION['id'] = $id;
-//             echo 'Welcome ' . $_SESSION['name'] . '!';
-//         } else {
-//             echo 'Incorrect username and/or password!';
-//         }
-//     } else {
-//         echo 'Incorrect username and/or password!';
-//     }
-
-//     $stmt->close();
-// }
-
 ?>
---
+++ moredetails.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>More Details</title>
+    <style>
+            /* Minified inline font-awesome preload */
+    .fa,.fab,.fal,.far,.fas{-moz-osx-font-smoothing:grayscale;-webkit-font-smoothing:antialiased;display:inline-block;font-style:normal;font-variant:normal;text-rendering:auto;line-height:1}.fa-balance-scale:before{content:"\f24e"}.fa-box:before{content:"\f466"}.fa-coins:before{content:"\f51e"}.fa-discord:before{content:"\f392"}.fa-check:before{content:"\f00c"}.fa-user-circle:before{content:"\f2bd"}.fa-gift:before{content:"\f06b"}.fa-thumbs-up:before{content:"\f164"}.fa-comment-dots:before{content:"\f4ad"}.fa-exchange-alt:before{content:"\f362"}.fa-video:before{content:"\f03d"}.fa-user-friends:before{content:"\f500"}.fa-external-link-alt:before{content:"\f35d"}.fa-language:before{content:"\f1ab"}.fa-check:before{content:"\f00c"}.fa-sort-down:before{content:"\f0dd"}.fa-bars:before{content:"\f0c9"}.fa-bullhorn:before{content:"\f0a1"}.fa-comments-dollar:before{content:"\f653"}.fa-gamepad:before{content:"\f11b"}.fa-hand-holding-usd:before{content:"\f4c0"}.fa-info:before{content:"\f129"}.fa-poll:before{content:"\f681"}.fa-flag:before{content:"\f024"}.fa-trophy:before{content:"\f091"}.fa-arrow-alt-circle-right:before{content:"\f35a"}.fa-layer-group:before{content:"\f5fd"}.fa-moon:before{content:"\f186"}.fa-plug:before{content:"\f1e6"}.fa-puzzle-piece:before{content:"\f12e"}.fa-question-circle:before{content:"\f059"}.fa-search:before{content:"\f002"}.fa-share-square:before{content:"\f14d"}.fa-signal:before{content:"\f012"}.fa-globe-americas:before{content:"\f57d"}.fa-globe-asia:before{content:"\f57e"}.fa-globe-europe:before{content:"\f7a2"}.fa-youtube:before{content:"\f167"}.fa-plus:before{content:"\f067"}.fa-book:before{content:"\f02d"}.fa-calendar-alt:before{content:"\f073"}.fa-clock:before{content:"\f017"}.fa-sort:before{content:"\f0dc"}.fa-sort-up:before{content:"\f0de"}.fa-sort-numeric-up:before{content:"\f163"}.fa-sort-numeric-down:before{content:"\f162"}.fa-star:before{content:"\f005"}.fa-award:before{content:"\f559"}.fa-chart-line:before{content:"\f201"}.fa-chart-pie:before{content:"\f200"}.fa-newspaper:before{content:"\f1ea"}.fa-times:before{content:"\f00d"}.fa-users:before{content:"\f0c0"}.fa-chevron-right:before{content:"\f054"}.fa-chevron-circle-left:before{content:"\f137"}.fa-fire-alt:before{content:"\f7e4"}.fa-edit:before{content:"\f044"}.fa-vr-cardboard:before{content:"\f729"}.fa-moon:before{content:"\f186"}.fa-trash:before{content:"\f1f8"}.fa-sun:before{content:"\f185"}.fa-chevron-up:before{content:"\f077"}.fa-fire:before{content:"\f06d"}.fa-random:before{content:"\f074"}.fa-file-download:before{content:"\f56d"}.fa-warehouse:before{content:"\f494"}@font-face{font-family:'Font Awesome 5 Brands';font-style:normal;font-weight:400;font-display:auto;src:url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/webfonts/fa-brands-400.woff2") format("woff2")}.fab{font-family:'Font Awesome 5 Brands'}@font-face{font-family:'Font Awesome 5 Free';font-style:normal;font-weight:400;font-display:auto;src:url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/webfonts/fa-regular-400.woff2") format("woff2")}.far{font-family:'Font Awesome 5 Free';font-weight:400}@font-face{font-family:'Font Awesome 5 Free';font-style:normal;font-weight:900;font-display:auto;src:url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/webfonts/fa-solid-900.woff2") format("woff2")}.fa,.fas{font-family:'Font Awesome 5 Free';font-weight:900}    
+    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="https://playtoearn.net/css/layouts/default.css" rel="stylesheet">
+    <link href="https://playtoearn.net/css/includes/header.css" rel="stylesheet">
+    <link href="https://playtoearn.net/css/pages/view.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="topad">
+            <div class="banner_advertise_top">
+                <a target="_blank" rel="nofollow noopener sponsored" href="https://revvracing.com/?utm_source=playtoearn.net&amp;utm_medium=banner+below+logo&amp;utm_campaign=playtoearn.net_banner+below+logo_play+to+earn+prizes">
+                <picture>
+                    <source media="(max-width: 767px)" srcset="https://playtoearn.net/img/banner/revvracing_mobile.gif">
+                    <source media="(min-width: 768px) and (max-width: 1399px)" srcset="https://playtoearn.net/img/banner/revvracing_tablet.gif">
+                    <source media="(min-width: 1400px)" srcset="https://playtoearn.net/img/banner/revvracing90.gif">
+                    <img src="https://playtoearn.net/img/banner/revvracing90.gif" id="banner_advertise_type1" alt="Banner Advertise">
+                </picture>
+                </a>
+                <div class="top_banner_info"><a href="https://playtoearn.net/advertise"><i class="fas fa-info"></i></a></div>
+            </div>
+        </div>
+        <div class="details">
+            <div>
+                <div>
+                    <div class="detail">
+                        <div class="row">
+                            <div class="col-12 col-lg-8 col-xl-9">
+                                <div>
+                                    <div class="dapp_profilepic">
+                                        <img loading="lazy" src="https://playtoearn.net/img/dapp/lucky-unicorn/profile_picture/100_lucky-unicorn.jpg" alt="Lucky Unicorn">
+                                    </div>
+
+                                    <div>
+                                        <div class="headline-dapp">
+                                            <h1>Lucky Unicorn</h1>
+                                            <div class="rank_container_float"><span class="total_rank rank">#36</span></div>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <p>Lucky Unicorn is a NFT Gaming &amp; NFT Trading platform where users could hatch eggs into different types of unicorns and use that to participate into multiple minigames. The core mechanism of the project is Play-to-Earn, meaning that players earn L99 while participating in the on-chain games. L99 token can be traded or used to enhance player&#039;s unicorn. Lucky Unicorn also utilize the Guild system which players could form or join different guilds to participate in Guild events &amp; activities.</p>
+                                    <hr />
+                                    <hr />
+                                        <div class="dapp_platforms">
+                                            <a data-toggle="tooltip" data-placement="top" title="Binance" href="https://playtoearn.net/blockchaingames/Binance/All-Genre/All-Status/All-Device/All-NFT/All-PlayToEarn"><div class="lazy platformicon Binance" data-loader="bgLoader"></div></a>
+                                        </div>
+                                        <div class="dapp_devices">
+                                            <a data-toggle="tooltip" data-placement="top" title="Web" href="https://playtoearn.net/blockchaingames/All-Blockchain/All-Genre/All-Status/Web/All-NFT/All-PlayToEarn"><div class="lazy deviceimg Web" data-loader="bgLoader"></div></a>
+                                        </div>
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                    <div class="dapp_nftlist">
+                                        <p class="related_dapp_h1">CarouselHere</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-4 col-xl-3">
+                            <hr class="mobileonly">
+                                <div class="social">
+                                    <a target="_blank" rel="nofollow noopener" href="https://luckyunicorn.io?utm_source=PlayToEarn.net&amp;utm_medium=organic&amp;utm_campaign=gamepage" class="btn btn-outline-success"><div class="lazy socialimg website" data-loader="bgLoader"></div></a>
+                                    <a target="_blank" rel="nofollow noopener" href="https://twitter.com/LuckyUnicornNFT" class="btn btn-outline-success"><div class="lazy socialimg twitter" data-loader="bgLoader"></div></a>
+                                    <a target="_blank" rel="nofollow noopener" href="https://discord.gg/Ry6W3QvcHk" class="btn btn-outline-success"><div class="lazy socialimg discord" data-loader="bgLoader"></div></a>
+                                    <a target="_blank" rel="nofollow noopener" href="https://t.me/LuckyUnicornGlobal" class="btn btn-outline-success"><div class="lazy socialimg telegram" data-loader="bgLoader"></div></a>
+                                    <a target="_blank" rel="nofollow noopener" href="https://reddit.com/r/luckyunicorn" class="btn btn-outline-success"><div class="lazy socialimg reddit" data-loader="bgLoader"></div></a>
+                                    <a target="_blank" rel="nofollow noopener" href="https://blog.luckyunicorn.io" class="btn btn-outline-success"><div class="lazy socialimg medium" data-loader="bgLoader"></div></a>
+                                    <a target="_blank" rel="nofollow noopener" href="https://docs.luckyunicorn.io" class="btn btn-outline-success"><div class="lazy socialimg gitbook" data-loader="bgLoader"></div></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bottommad">
+                        <div class="banner_advertise_bottom_ad">
+                            <div class="bsa_container_helper-bottom"><div id="bsa-zone_1622573644942-5_123456"></div></div>
+                        </div>
+                        <div class="banner_advertise_bottom">
+                            <a target="_blank" rel="nofollow noopener sponsored" href="https://doctorwho-worldsapart.com/exclusive-token-sale?utm_source=display&amp;utm_medium=cpc&amp;utm_campaign=PlayToEarn_Jan&amp;utm_id=DoctorWho">
+                            <picture>
+                                <source media="(max-width: 767px)" srcset="https://playtoearn.net/img/banner/doctorwho2_mobile.png">
+                                <source media="(min-width: 768px) and (max-width: 1399px)" srcset="https://playtoearn.net/img/banner/doctorwho2_tablet.png">
+                                <source media="(min-width: 1400px)" srcset="https://playtoearn.net/img/banner/doctorwho290.png">
+                                <img loading="lazy" src="https://playtoearn.net/img/banner/doctorwho290.png" id="banner_advertise_type3" alt="Banner Advertise">
+                            </picture>
+                            </a>
+                            <div class="top_banner_info"><a href="https://playtoearn.net/advertise"><i class="fas fa-info"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scroll-to-top">
+            <button class="btn" aria-label="ScrollToTop"><span class="fa fa-chevron-up"></span></button>
+            </hr>
+        </div>
+    </div>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+    <script>
+
+        $(function(){
+            // If media query mobile
+            var mediaQuery = window.matchMedia("(max-width: 768px)");
+            $('.deviceimg').addClass('loaded');
+            $('.platformicon').addClass('loaded');
+            $('.socialimg').addClass('loaded');
+            $('.socialimgfooter').addClass('loaded');
+            $('.virtualworldicon').addClass('loaded');
+
+            $('.banner_advertise_top_ad').css('display', 'none');            
+            $('.banner_advertise_toppagetext').css('display', 'none');
+            $('.banner_advertise_toppagetext_replace').addClass('banner_advertise_toppagetext_active');
+            $('.banner_advertise_bottom_ad').css('display', 'none');
+            $('.banner_advertise_bottom').css('display', 'flex');
+            $('.sidebar_banner').css('display', 'block');
+            $('.report_edit_container').addClass('cz-active');
+            $('.report_edit_container').css('display', 'block');
+
+            // add button in front of button end
+            var back_to_top_button = ['<a href="#top" class="scroll-to-top" aria-label="Scroll to top"></a>'].join("");
+            $('body').append(back_to_top_button);
+
+            // hide button
+            $('.scroll-to-top').hide();
+
+            // scroll function
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 50) { // after 50px scrolled
+                    $('.scroll-to-top').fadeIn();
+                } else {
+                    $('.scroll-to-top').fadeOut();
+                }
+            });
+
+            $('.scroll-to-top .btn').click(function () { // click on button
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 800);
+                return false;
+            });
+        });
+    </script>
+</body>
+</html>
