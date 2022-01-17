44e2b6a308d4dd99b7818c37525b34e0feb5a4ea
tharitthaveekittikul <s6301012630079@email.kmutnb.ac.th>
2022-01-17 21:55:43 +0700

    update css page 2

---
+++ details.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>More Details</title>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"> -->
+
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <div class="container-object">
+        <picture>
+            <source media="(max-width: 500px)" srcset="images/ads_1_small.gif">
+            <source media="(min-width: 1370px)" srcset="images/ads_1_big.gif">
+            <img src="images/ads_1_big.gif" alt="banner ads">
+        </picture>
+        <!-- <h1>Hi</h1> -->
+        <div class="all-content">
+            <div class="text-content">
+                <h1>Google</h1>
+                <div class="clearfix"></div>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type 
+                specimen book. It has survived not only five centuries, but also the leap into 
+                electronic typesetting, remaining essentially unchanged. It was popularised in 
+                the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more</p>
+            </div>
+            <div class="ads-banner">
+                <img src="images/ads_2.png" alt="banner ads2">
+            </div>
+            <div class="clearfix"></div>
+            <div class="social-network">
+                <div class="network">
+                    <h2>Network : </h2>
+                </div>
+                <div class="social">
+                    <h2>Social : </h2>
+                </div>
+            </div>
+            
+            <div class="clearfix"></div>
+            <br>
+            <div class="carousel-content">
+              <div class="row">
+                <div class="col-md-12">
+                    <div class="carousel slide multi-item-carousel" id="theCarousel">
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?girl"></a></div>
+                            </div>
+                            <div class="item">
+                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?bikni"></a></div>
+                            </div>
+                            <div class="item">
+                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?beauty"></a></div>
+                            </div>
+                            <div class="item">
+                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?yoga"></a></div>
+                            </div>
+                            <div class="item">
+                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?natural"></a></div>
+                            </div>
+                            <div class="item">
+                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?fitness"></a></div>
+                            </div>
+                            <!-- add  more items here -->
+                            <!-- Example item start:  -->
+                            
+                            <div class="item">
+                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?cycling"></a></div>
+                            </div>
+                            
+                            <!--  Example item end -->
+                            </div>
+                            <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
+                            <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+                    </div>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <picture>
+            <source media="(max-width: 500px)" srcset="images/ads_1_small.gif">
+            <source media="(min-width: 1370px)" srcset="images/ads_1_big.gif">
+            <img src="images/ads_1_big.gif" alt="banner ads">
+        </picture>
+    </div>
+        
+    
+        
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script  src="index.js"></script> -->
+
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
+    <script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'></script>
+    <script  src="js/details.js"></script>
+</body>
+</html>
