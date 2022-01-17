305e7d3a5c7f131afb27de787f10242d5ca1596f
tharitthaveekittikul <s6301012630079@email.kmutnb.ac.th>
2022-01-18 01:33:55 +0700

    delete comment

--- details.php
+++ details.php
@@ -78,40 +78,6 @@
                         </div>
                     </div>
                 </div>
-                <!-- <div class="carousel-content">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="carousel slide multi-item-carousel" id="theCarousel">
-                            <div class="carousel-inner">
-                                <div class="item active">
-                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?girl"></a></div>
-                                </div>
-                                <div class="item">
-                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?bikni"></a></div>
-                                </div>
-                                <div class="item">
-                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?beauty"></a></div>
-                                </div>
-                                <div class="item">
-                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?yoga"></a></div>
-                                </div>
-                                <div class="item">
-                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?natural"></a></div>
-                                </div>
-                                <div class="item">
-                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?fitness"></a></div>
-                                </div>
-                                
-                                <div class="item">
-                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?cycling"></a></div>
-                                </div>
-                                </div>
-                                <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-                                <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-                </div> -->
             </div>
         </div>
 
