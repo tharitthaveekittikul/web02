3db2cba1b16d63cb31842a3aca2ac517de114daa
tharitthaveekittikul <s6301012630079@email.kmutnb.ac.th>
2022-01-18 01:31:24 +0700

    update page 2

--- details.php
+++ details.php
@@ -8,92 +8,123 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"> -->
 
-    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 <body>
-    <div class="container-object">
-        <picture>
-            <source media="(max-width: 500px)" srcset="images/ads_1_small.gif">
-            <source media="(min-width: 1370px)" srcset="images/ads_1_big.gif">
-            <img src="images/ads_1_big.gif" alt="banner ads">
-        </picture>
-        <!-- <h1>Hi</h1> -->
-        <div class="all-content">
-            <div class="text-content">
-                <h1>Google</h1>
-                <div class="clearfix"></div>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of type and scrambled it to make a type 
-                specimen book. It has survived not only five centuries, but also the leap into 
-                electronic typesetting, remaining essentially unchanged. It was popularised in 
-                the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more</p>
-            </div>
-            <div class="ads-banner">
-                <img src="images/ads_2.png" alt="banner ads2">
-            </div>
-            <div class="clearfix"></div>
-            <div class="social-network">
-                <div class="network">
-                    <h2>Network : </h2>
-                </div>
-                <div class="social">
-                    <h2>Social : </h2>
-                </div>
-            </div>
-            
-            <div class="clearfix"></div>
-            <br>
-            <div class="carousel-content">
-              <div class="row">
-                <div class="col-md-12">
-                    <div class="carousel slide multi-item-carousel" id="theCarousel">
-                        <div class="carousel-inner">
-                            <div class="item active">
-                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?girl"></a></div>
-                            </div>
-                            <div class="item">
-                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?bikni"></a></div>
-                            </div>
-                            <div class="item">
-                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?beauty"></a></div>
-                            </div>
-                            <div class="item">
-                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?yoga"></a></div>
-                            </div>
-                            <div class="item">
-                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?natural"></a></div>
-                            </div>
-                            <div class="item">
-                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?fitness"></a></div>
-                            </div>
-                            <!-- add  more items here -->
-                            <!-- Example item start:  -->
-                            
-                            <div class="item">
-                                <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?cycling"></a></div>
-                            </div>
-                            
-                            <!--  Example item end -->
-                            </div>
-                            <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-                            <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+    <div class="allthing">
+        <div class="header-top">
+            <picture>
+                <source media="(max-width: 500px)" srcset="images/ads_1_small.gif">
+                <source media="(min-width: 1370px)" srcset="images/ads_1_big.gif">
+                <img src="images/ads_1_big.gif" alt="banner ads">
+            </picture>
+        </div>
+        <div class="container-object">
+            <!-- <h1>Hi</h1> -->
+            <div class="all-content">
+                <div class="text-ads">
+                    <div class="text-content">
+                        <h1>Google</h1>
+                        <hr>
+                        <div class="clearfix"></div>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type 
+                        specimen book. It has survived not only five centuries, but also the leap into 
+                        electronic typesetting, remaining essentially unchanged. It was popularised in 
+                        the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more</p>
+                    </div>
+                    <div class="ads-banner">
+                        <img src="images/ads_2.png" alt="banner ads2">
                     </div>
                 </div>
-              </div>
+                
+                <hr>
+                <div class="clearfix"></div>
+                <div class="social-network">
+                    <div class="network" style="margin-top:20px">
+                        <h2>Network : </h2>
+                    </div>
+                    <div class="social" style="margin-top:20px">
+                        <h2>Social : </h2>
+                    </div>
+                </div>
+                <hr>
+                <!-- <div class="clearfix"></div> -->
+                <div class="container-carousel">
+                    <div id="slider" class="owl-carousel">
+                        <div class="content-area">
+                            <img src="https://source.unsplash.com/300x300/?girl" alt="">
+                        </div>
+                        <div class="content-area">
+                            <img src="https://source.unsplash.com/300x300/?bikni" alt="">
+                        </div>
+                        <div class="content-area">
+                            <img src="https://source.unsplash.com/300x300/?beauty" alt="">
+                        </div>
+                        <div class="content-area">
+                            <img src="https://source.unsplash.com/300x300/?cat" alt="">
+                        </div>
+                        <div class="content-area">
+                            <img src="https://source.unsplash.com/300x300/?dog" alt="">
+                        </div>
+                        <div class="content-area">
+                            <img src="https://source.unsplash.com/300x300/?yoga" alt="">
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="carousel-content">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="carousel slide multi-item-carousel" id="theCarousel">
+                            <div class="carousel-inner">
+                                <div class="item active">
+                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?girl"></a></div>
+                                </div>
+                                <div class="item">
+                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?bikni"></a></div>
+                                </div>
+                                <div class="item">
+                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?beauty"></a></div>
+                                </div>
+                                <div class="item">
+                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?yoga"></a></div>
+                                </div>
+                                <div class="item">
+                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?natural"></a></div>
+                                </div>
+                                <div class="item">
+                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?fitness"></a></div>
+                                </div>
+                                
+                                <div class="item">
+                                    <div class="col-xs-4"><a href="#1"><img class="carousel-inner-img" src="https://source.unsplash.com/300x300/?cycling"></a></div>
+                                </div>
+                                </div>
+                                <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
+                                <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                </div> -->
             </div>
         </div>
-    </div>
 
-    <div class="footer">
-        <picture>
-            <source media="(max-width: 500px)" srcset="images/ads_1_small.gif">
-            <source media="(min-width: 1370px)" srcset="images/ads_1_big.gif">
-            <img src="images/ads_1_big.gif" alt="banner ads">
-        </picture>
+        <div class="footer-style">
+            <picture>
+                <source media="(max-width: 500px)" srcset="images/ads_1_small.gif">
+                <source media="(min-width: 1370px)" srcset="images/ads_1_big.gif">
+                <img src="images/ads_1_big.gif" alt="banner ads">
+            </picture>
+        </div>
     </div>
+    
+    
         
     
         
@@ -104,5 +135,8 @@
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
     <script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'></script>
     <script  src="js/details.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="js/owl-carousel.js"></script>
+    <script src="js/arrow-carousel.js"></script>
 </body>
 </html>
---
+++ js/owl-carousel.js
@@ -0,0 +1,19 @@
+$('#slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+})
+
